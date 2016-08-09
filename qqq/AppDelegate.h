c93e6591f11ab77ee47132a9c94ec3b3f21981fa
创新创业中心 <cxcy@cxcy-imac.local>
2016-08-09 16:58:41 +0800

    Initial Commit

---
+++ qqq/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qqq
+//
+//  Created by 创新创业中心 on 16/8/9.
+//  Copyright © 2016年 UESTCACM QKTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qqq/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  qqq
+//
+//  Created by 创新创业中心 on 16/8/9.
+//  Copyright © 2016年 UESTCACM QKTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
